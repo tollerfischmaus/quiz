e8ba72b38b2a423b0bc203cf29cda80cfe2d8436
tollerfischmaus <robin.warnecke@gysalzhausen.de>
2025-04-04 13:11:21 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,90 @@
-function startani () {
+function schwer () {
 	
 }
+function startani () {
+    music.playMelody("C D E F G A B C5 ", 203)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # . . . #
+        # . . . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(100)
+}
+function menü () {
+    basic.showString("menu")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        # . # . #
+        # # # . #
+        # . # . #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # # . . #
+        # . # . #
+        # # . . #
+        # . # . #
+        # # . . #
+        `)
+    basic.pause(2000)
+    while (!(input.buttonIsPressed(Button.A)) && !(input.buttonIsPressed(Button.B))) {
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            # . . . #
+            # . . . #
+            # . . . #
+            `)
+        basic.pause(100)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.setLedColor(0xff0000)
+        basic.pause(2000)
+        schwer()
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(2000)
+        basic.setLedColor(0x00ff00)
+        leicht()
+    }
+}
+function leicht () {
+	
+}
+startani()
+menü()
 basic.forever(function () {
 	
 })
